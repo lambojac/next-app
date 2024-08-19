9077f2fe2c015eb5efb1f4374b6b9e5b002723bc
adamokennie <adamokennie@gmail.com>
2024-08-19 12:05:33 +0100

    search button

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { BsSearch } from "react-icons/bs";
 const HeaderMain = () => {
   return (
-    <div className='border-b border-gray-200 py-6'>
-      <div className='container sm:flex justify-between items-center'>
-       <div className='font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish'>
-logo
-       </div>
-       <div className='w-full sm:w-[300px] md:w-[70%] relative'>
-<input className="border-gray-200 border p-2 px-4 rounded-lg w-full" type="text" placeholder="Enter any product name..."/>
-       </div>
+    <div className="border-b border-gray-200 py-6">
+      <div className="container sm:flex justify-between items-center">
+        <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish">
+          logo
+        </div>
+        <div className="w-full sm:w-[300px] md:w-[70%] relative">
+          <input
+            className="border-gray-200 border p-2 px-4 rounded-lg w-full"
+            type="text"
+            placeholder="Enter any product name..."
+          />
+          <BsSearch className="absolute right-0 top-0 mr-3 mt-3 text-gray-400" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderMain
+export default HeaderMain;
