340e1960c307f873574305ebb65614ea356555ee
adamokennie <adamokennie@gmail.com>
2024-08-19 10:09:02 +0100

    drop down

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,28 +1,49 @@
 import React from "react";
-import {BsFacebook,BsTwitter,BsInstagram, BsLinkedin} from "react-icons/bs"
+import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 const HeaderTop = () => {
   return (
-    <div className="border-b border-gray-200 hidden sm:block ">
+    <div className="border-b border-gray-200 hidden sm:block">
       <div className="container py-4">
         <div className="flex justify-between items-center">
           <div className="hidden lg:flex gap-1">
             <div className="header_top_icon_wrapper">
-             <BsFacebook/>
+              <BsFacebook />
             </div>
             <div className="header_top_icon_wrapper">
-<BsTwitter/>
+              <BsTwitter />
             </div>
             <div className="header_top_icon_wrapper">
-<BsInstagram/>
+              <BsInstagram />
             </div>
             <div className="header_top_icon_wrapper">
-<BsLinkedin/>
+              <BsLinkedin />
             </div>
           </div>
 
           <div className="text-gray-500 text-[12px]">
-<b> FREE SHIPPING </b> THIS WEEK ORDER OVER -50$
+            <b>FREE SHIPPING</b> THIS WEEK ORDER OVER -50$
+          </div>
+
+          <div className="flex gap-4">
+            <select
+              className="text-gray-500 text-[12px] w-[70px]"
+              name="currency"
+              id="currency"
+            >
+              <option value="USD $">USD $</option>
+              <option value="EUR $">EUR $</option>
+              <option value="INR $">INR $</option>
+            </select>
+            <select
+              className="text-gray-500 text-[12px] w-[80px]"
+              name="language"
+              id="language"
+            >
+              <option value="english">EN</option>
+              <option value="french">FR</option>
+              <option value="espanol">ES</option>
+            </select>
           </div>
         </div>
       </div>
