350ff6508d15d4cfe59a51d84ed99c82da78dca5
adamokennie <adamokennie@gmail.com>
2024-08-19 11:55:57 +0100

    search button

---
+++ src/components/HeaderMain.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeaderMain = () => {
+  return (
+    <div className='border-b border-gray-200 py-6'>
+      <div className='container sm:flex justify-between items-center'>
+       <div className='font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish'>
+logo
+       </div>
+       <div className='w-full sm:w-[300px] md:w-[70%] relative'>
+<input className="border-gray-200 border p-2 px-4 rounded-lg w-full" type="text" placeholder="Enter any product name..."/>
+       </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderMain
