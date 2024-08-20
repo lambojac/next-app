4c02e3cabbe08453a6d8d26e528bc87e22490d32
adamokennie <adamokennie@gmail.com>
2024-08-20 12:35:24 +0100

    frontend

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,13 @@ const Navbar = () => {
       <div className="container">
         <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
           <Link href="/" className="navbar__link">Home</Link>
-          <Link href="/about" className="navbar__link">About</Link>
-          <Link href="/contact" className="navbar__link">Contact</Link>
+          <Link href="/about" className="navbar__link">Categories</Link>
+          <Link href="/contact" className="navbar__link">MEN'S</Link>
+          <Link href="/contact" className="navbar__link">WOMEN'S</Link>
+          <Link href="/contact" className="navbar__link">JEWELRY</Link>
+          <Link href="/contact" className="navbar__link">PERFUME</Link>
+          <Link href="/contact" className="navbar__link">BLOG</Link>
+          <Link href="/contact" className="navbar__link">NOT OFFERS</Link>
         </div>
       </div>
     </div>
